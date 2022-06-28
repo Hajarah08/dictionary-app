4c052c92dd7df506fbf57ceeccd997b4e9587412
Hajarah08 <koikiadetutu@gmail.com>
2022-06-28 09:16:54 +0000

    added word meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,29 +1,34 @@
 import React, { useState} from "react";
 import "./Dictionary.css";
-import axios from "axios"
+import axios from "axios";
+import Results from "./Results"
 
 export default function Dictionary(){
     let [keyword, setKeyword]= useState("");
+     let [results, setResults] = useState(null);
 
-    function handleSubmit(response){
-console.log(response.data)
+    function handleResponse(response){
+
+ setResults(response.data[0]);
+
     }
 
     function search(event){
         event.preventDefault()
         
 
-        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_us/${keyword}`;
-        axios.get(apiUrl).then(handleSubmit);
+        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
     function handleKeywordChange(event){
         setKeyword(event.target.value);
     }
     return (
-        <div className="Dictionary">
-            <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange}/>
-            </form>
-        </div>
-    )
+      <div className="Dictionary">
+        <form onSubmit={search}>
+          <input type="search" onChange={handleKeywordChange} />
+        </form>
+        <Results results={results} />
+      </div>
+    );
 }
